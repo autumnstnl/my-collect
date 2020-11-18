7d1d6d1b6882a26f2d1f2ec72d147722a8ae7ea7
Autumn <autumnstnl@gmail.com>
2020-11-18 11:39:43 -0500

    commit tree 331faba7b534faa5c9348fa4ec6b207992bb1604

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
-
-
+def my_collect(collection)
+    count = 0 
+    my_collect = []
+    while count < collection.length
+        my_collect << yield(collection[count])
+        count += 1
+        end
+     my_collect
+    
+end
+    language = []
+    my_collect(language) do |language| my language.upcase
+        puts language
+end
+# (my_collect(languages) do |language|
+# language.upcase
+# end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
